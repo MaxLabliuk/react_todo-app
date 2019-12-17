9e60534f656c62ef543448b6cf7db1958b7f3de1
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-12-17 09:37:30 +0200

    Updates

---
+++ precache-manifest.4dbeabe81b22b350b9fc8cd901c6bab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53eb95a2ba351d7cca9e2ae4b3d05dca",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "66d348e15c729e793a90",
+    "url": "/react_todo-app/static/css/main.9eb11c57.chunk.css"
+  },
+  {
+    "revision": "8c1ca4089b4f1d17e5c4",
+    "url": "/react_todo-app/static/js/2.44edda1a.chunk.js"
+  },
+  {
+    "revision": "66d348e15c729e793a90",
+    "url": "/react_todo-app/static/js/main.ddb09952.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
